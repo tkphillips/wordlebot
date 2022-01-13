e5a573ea28b4cad103955fe6b624a0627e444b02
Tristan Phillips <tkphillips@crimson.ua.edu>
2022-01-13 16:17:15 -0600

    basic framework

--- bot.cpp
+++ bot.cpp
@@ -15,8 +15,29 @@ Gray letters are not in the word
 You only get 6 guesses and all guesses have to be real words
 ***************************************************************************/
 
+struct green{
+    int pos;
+    char letter;
+};
+
+vector<string> findAcceptableWords(vector<green> green, vector<char> yellow, vector<char> gray){
+    vector<string> words;
+    //find words that work
+
+    //return vector of those words
+    return words;
+}
+
+string findOptimalWordInList(vector<string> words){
+    string bestWord;
+    //find best word
+
+    //return best word
+    return bestWord;
+}
+
 int main () {
-    //Guess TRACE
-    
+    //Guess TRACE???
+
     return 0;
 }
