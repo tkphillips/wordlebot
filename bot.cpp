61a59cc746e1bb2371b4fc0aff8ba6be3dd4cd3e
Tristan Phillips <tkphillips@crimson.ua.edu>
2022-01-13 15:49:48 -0600

    Created Wordle Bot

---
+++ bot.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <string>
+#include<sstream>
+using namespace std;
+
+/*************************************************************************
+WORDLE:
+The word can be any five letter word.
+Green letters are in the correct possition 
+Yellow letters are not in the correct position but are in the word
+Gray letters are not in the word
+You only get 6 guesses and all guesses have to be real words
+***************************************************************************/
+
+int main () {
+    //Guess TRACE
+    
+    return 0;
+}
